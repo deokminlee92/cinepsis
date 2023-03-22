271dc155a5e074e09c3c591d9038e57a1c031aa0
deokminlee92 <deokminlee92@gmail.com>
2023-03-23 00:16:42 +0100

    searchBar construction start

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { useState } from "react";
+import movies from "../assets/db.json";
 
 function SearchBar() {
-  return <input></input>;
+  //useState
+  const [search, setSearch] = useState("");
+
+  //Mettre à jour la valeur entrée
+  const onChange = (e) => {
+    setSearch(e.target.value);
+  };
+  const filterTitle = movies.filter((p) => {
+    return p.title.toLocaleLowerCase().includes(search.toLocaleLowerCase());
+  });
+
+  return (
+    <div className="SearchBar-container">
+      <input
+        className="SearchBar"
+        type="text"
+        value={search}
+        onChange={onChange}
+      ></input>
+      ;
+    </div>
+  );
 }
 
 export default SearchBar;
